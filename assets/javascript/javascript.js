985124c23547321ff9266d31f3601a53b03a5689
Campbell,Ian <icampbell9615@gmail.com>
2019-07-02 16:21:00 -0500

    Commit for train

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     var firebaseConfig = {
         apiKey: "AIzaSyDO0H87iPV-gb6wvW2YCCt9PAc45MfqYeI",
         authDomain: "train-scheduler-c034a.firebaseapp.com",
@@ -8,24 +8,24 @@ $( document ).ready(function() {
         messagingSenderId: "1004870621281",
         appId: "1:1004870621281:web:24872452b0cfbf51"
       };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
- 
-
-     var database = firebase.database();
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    var database = firebase.database();
+   
     
     //Submit given info
-    $("#infoTrain").on("click", function(event) {
+    $("#addTrainBtn").on("click", function(event) {
         event.preventDefault(); //no button reset
-    
-    //Set user input values 
-        var trainName = $("#name").val().trim();
-        var destination = $("#dest").val().trim();
-    
         
-        var firstTime = moment($("#firstTime").val().trim(), "hh:mm").subtract(1, "years").format("X");
-    
-        var frequency = $("#freq").val().trim();
+  // Your web app's Firebase configuration
+
+        console.log("hello")
+    //Set user input values 
+        var trainName = $("#trainName").val().trim();
+        var destination = $("#dest").val().trim();
+        var firstTime = $("#firstTrain").val().trim();
+        var frequency = $("#frequency").val().trim();
     
     
         //Get all the new train info
@@ -35,9 +35,65 @@ $( document ).ready(function() {
             trainGoing: destination,
             trainComing: firstTime,
             everyXMin: frequency
+
         };
-    
+
+        database.ref().push(newTrain);
+
+        console.log(newTrain.train);
+        console.log(newTrain.trainGoing);
+        console.log(newTrain.trainComing);
+        console.log(newTrain.everyXMin);
+
+        trainName= $("#trainName").val("");
+        destination = $("#dest").val("");
+        firstTime = $("#firstTrain").val("");
+        frequency = $("#frequency").val("");
+
     });
-    });
+
     
+        database.ref().on("child_added", function(childSnapshot){
+            console.log(childSnapshot)
+            /*Create variables to store the data inside of*/
+            var locoName = childSnapshot.val().name;
+            var locoDestination = childSnapshot.val().destination;
+            var locoFrequency = childSnapshot.val().frequency;
+            var locoFirstTrain = childSnapshot.val().firstTrain;
+        
+                /*Create variables for time storage*/
+                var firstTimeTransfered = moment(locoFirstTrain, "hh:mm").subtract(1, "years");
+                    console.log(firstTimeTransfered);
+        
+                var presentTime = moment();
+                    console.log("Current Time: " + moment(presentTime).format("hh:mm"));
+        
+                var differenceInTimes = moment().diff(moment(firstTimeTransfered), "minutes");
+                    console.log("Time difference: " + differenceInTimes);
+        
+                var timeRemainder = differenceInTimes % locoFrequency;
+                    console.log(timeRemainder);
+        
+                var minutesTilTrain = locoFrequency - timeRemainder;
+                    console.log("Minutes til train: " + minutesTilTrain);
+        
+                var nextTrain = moment().add(minutesTilTrain, "minutes");
+                    console.log("Next train arrival: " + moment(nextTrain).format("hh:mm"));
+        
+                var nextTime = moment(nextTrain).format("hh:mm");
+        
+            /*Add the train data into table*/
+            $("tbody").append("<tr><td>" + locoName + "</td><td>" + locoDestination + "</td><td>" + locoFrequency + "</td><td>" + nextTime + "</td><td>" + minutesTilTrain + "</td></tr>");
+        
+        /*Childsnapshot and prevchildkey function*/
+        });
+        
+        /*close function*/
+        
+
+
+
+});
+    
+
     
